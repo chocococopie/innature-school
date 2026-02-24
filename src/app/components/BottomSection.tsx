a2bf756bf269d17300ccdb9df62e893130052e3e
chocococopie <dshagiakh@gmail.com>
2026-02-24 13:49:04 +0100

    fix fonts

--- src/app/components/BottomSection.tsx
+++ src/app/components/BottomSection.tsx
@@ -9,7 +9,7 @@ const BottomSection: React.FC = () => {
       <TextBlock
         className="bottom-text"
         title="Über uns"
-        text="Third Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+        text="Third Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
       />
       <div className="people-photo">
         <Image
